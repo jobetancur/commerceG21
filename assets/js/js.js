18382db073cdd30b34904243088ca282b39c9409
Alejandro Betancur <alejobetancur2@gmail.com>
2022-12-06 20:54:51 -0500

    new project

---
+++ assets/js/js.js
@@ -0,0 +1,29 @@
+import { getProducts, addProduct } from './crud.js';
+
+getProducts();
+
+window.addProduct = addProduct
+
+// Guardar elementos en el local storage:
+//localStorage.setItem("name", "Alejandro")
+//localStorage.setItem("lastname", "Betancur")
+
+// Obtener un valor del LocalStorage
+//console.log( localStorage.getItem("name") );
+
+// Eliminar un valor del Local Storage
+//localStorage.removeItem("lastname")
+
+// Limpiar todas las claves del Local Storage
+//localStorage.clear()
+
+//const user = { name: "Pepito" }
+
+// Convertir el objeto a string para poder guardarlo.
+//localStorage.setItem("user", JSON.stringify(user))
+
+//const userFromLocalStorage = localStorage.getItem("user")
+
+//console.log(userFromLocalStorage)
+
+//console.log(JSON.parse(userFromLocalStorage))
